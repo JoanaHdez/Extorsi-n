bcc92080fef5c8ab6e2d39adfc32087c44e503b1
JoanaHdez <ciahdez22@gmail.com>
2026-05-20 13:44:21 -0600

    v-200526-1344

---
+++ Extorsion/app/Config/Routes.php
@@ -0,0 +1,10 @@
+<?php
+
+/* namespace Config;
+ */use CodeIgniter\Router\RouteCollection;
+/* use Config\Services;
+ */
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
